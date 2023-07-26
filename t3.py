c92d6d8b0709723bacb6ca362700c85e1afe3f66
Akash Kumar Singh <akashkumarsingh1191@gmail.com>
2023-07-26 19:48:42 +0530

    for loop

---
+++ t3.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+pensize(2)
+speed("slow")
+bgcolor('black')
+pencolor('red')
+width(1)
+for i in range(6):
+    lt(60)
+    fd(100)
+    for i in range(6):
+        lt(60)
+        fd(50)
+        fillcolor('blue')
+        begin_fill()
+        
+hideturtle()
+mainloop()
