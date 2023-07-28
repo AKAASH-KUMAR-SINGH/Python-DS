a45483066422a54df99f3143cea66e54f12f200a
Akash Kumar Singh <akashkumarsingh1191@gmail.com>
2023-07-28 19:46:21 +0530

    while loop

---
+++ login.py
@@ -0,0 +1,19 @@
+uname='Admin'
+pwd='Admin@123'
+attempt=0
+while True:
+    attempt +=1
+    print(f'Attempt {attempt} of 3')
+    username=input("Enter username :")
+    password=input("Enter the password :")
+    if attempt==2:
+        print('Too many attempt')
+        break
+    if uname != username:
+        print("Invalid username")
+    if pwd != password:
+        print("Invalid password")
+    if uname == username and pwd ==password:
+        print('Login Sucessfull')
+        break
+    
---
+++ summer.py
@@ -0,0 +1,5 @@
+print('Welcome to summer')
+ans=0
+while input('continue addind value [y]')=='y':
+    ans+=int(input('enter a number'))
+print('the sum is :',ans)
