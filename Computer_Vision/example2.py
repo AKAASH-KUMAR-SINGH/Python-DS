b394110f0e1f395c116cf08fb4ba4bdca99e520e
Akash <akashkumarsingh1191@gmail.com>
2023-09-30 21:35:47 +0530

    opencv3

--- Computer_Vision/example2.py
+++ Computer_Vision/example2.py
@@ -1,24 +1,17 @@
 import cv2
-cap=cv2.VideoCapture(0)
-fourcc=cv2.VideoWriter_fourcc(*'XVID')
-out=cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+img=cv2.imread('H:\Python+DS\Computer_Vision\img.jpg',-1)
+# Draw a line on the image
+# syntax:(img, (initial coordinate),(final coordinate),(color in RGB--> 'BGR'),thickness)
+cv2.line(img,(0,0),(255,255),(255,245,255),10)
+# arrowedLine(img,(initial coordinate),(final coordinate),(RGB color),thickness)
+cv2.arrowedLine(img,(0,0),(210,230),(0,0,253),20)
+# rectangle
+cv2.rectangle(img,(240,120),(210,20),(200,243,56),10)
+# circle
+cv2.circle(img,(222,123),43,(234,43,123),0)
+font=cv2.FONT_HERSHEY_SIMPLEX
+img=cv2.putText(img,'AKASH',(10,500),font,4,(234,255,255),3,cv2.LINE_AA)
 
-while (True):
-    ret, frame = cap.read()
-    if ret==True:
-        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        
-        out.write(frame)
-
-        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame',frame)
-
-        if cv2.waitKey(1) &0xFF ==ord('q'):
-            break
-    else:
-        break
-
-cap.release()
-out.release()
+cv2.imshow('Window',img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
